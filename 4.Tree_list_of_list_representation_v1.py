dfdf20adc77435bb5ad0054990c0d484741645fc
JangIjal <rizalubuntuuser@gmail.com>
2019-06-13 07:17:51 +0700

    tree data structure using list representation

---
+++ 4.Tree_list_of_list_representation_v1.py
@@ -0,0 +1,6 @@
+# sample tree data structure using list representation
+myTree = ['a', ['b', ['d', [], []], ['e', [], []]], ['c', ['f', [], []], []]]
+print(myTree)
+print('left subtree = ', myTree[1])
+print('root = ', myTree[0])
+print('right subtree = ', myTree[2])
